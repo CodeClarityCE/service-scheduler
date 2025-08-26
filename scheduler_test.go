4f045e07650f0c79e003bca59714bef60d792cca
CedricHerzog <CedricHerzog@users.noreply.github.com>
2025-08-26 08:42:38 +0200

    Format

--- scheduler_test.go
+++ scheduler_test.go
@@ -11,7 +11,7 @@ func TestCreateSchedulerService(t *testing.T) {
 	os.Setenv("PG_DB_PORT", "5432")
 	os.Setenv("PG_DB_USER", "postgres")
 	os.Setenv("PG_DB_PASSWORD", "!ChangeMe!")
-	
+
 	// Test scheduler service creation
 	service, err := CreateSchedulerService()
 	if err != nil {
@@ -19,22 +19,22 @@ func TestCreateSchedulerService(t *testing.T) {
 		return
 	}
 	defer service.Close()
-	
+
 	// Test that service was created successfully
 	if service == nil {
 		t.Error("Expected service to be created, got nil")
 	}
-	
+
 	// Test that ServiceBase is properly embedded
 	if service.ServiceBase == nil {
 		t.Error("Expected ServiceBase to be embedded, got nil")
 	}
-	
+
 	// Test that cron scheduler is initialized
 	if service.cron == nil {
 		t.Error("Expected cron scheduler to be initialized, got nil")
 	}
-	
+
 	// Test that API URL is set
 	if service.apiURL == "" {
 		t.Error("Expected API URL to be set, got empty string")
